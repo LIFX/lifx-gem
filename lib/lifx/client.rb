e1cc75d9eb5d928ee251ea5dd3df463ddfea4164
Jack Chen (chendo) <github@chen.do>
2014-02-19 10:32:16 +1100

    Smarter discovery timeout

--- lib/lifx/client.rb
+++ lib/lifx/client.rb
@@ -1,4 +1,5 @@
 require 'socket'
+require 'timeout'
 
 module LIFX
   class Client
@@ -13,12 +14,19 @@ module LIFX
       end
     end
 
-    DISCOVERY_DEFAULT_TIMEOUT = 0.5
+    DISCOVERY_DEFAULT_TIMEOUT = 5
     def discover(timeout = DISCOVERY_DEFAULT_TIMEOUT)
-      @networks.each do |network|
-        network.discover
+      Timeout.timeout(timeout) do
+        @networks.each do |network|
+          network.discover
+        end
+        while sites.empty? || sites.none? { |s| s.gateway }
+          sleep 0.1
+        end
+        sites
       end
-      sleep(timeout)
+    rescue Timeout::Error
+      sites
     end
 
     def sites
