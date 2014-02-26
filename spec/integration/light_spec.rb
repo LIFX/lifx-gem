c683a52997e4028e46eb464024a63b9eb92b6010
Jack Chen (chendo) <github@chen.do>
2014-02-27 10:34:21 +1100

    Refactor spec helpers so we can use them in Group

--- spec/integration/light_spec.rb
+++ spec/integration/light_spec.rb
@@ -1,39 +1,14 @@
 require 'spec_helper'
 
 module LIFX
-  describe Light do
-    before(:all) do
-      $lifx = LIFX::Client.new(logger: Yell.new(STDERR))
-      $lifx.discover
-      begin
-        Timeout.timeout(5) do
-          while $lifx.lights.empty?
-            sleep 0.5
-          end
-        end
-      rescue Timeout::Error
-        raise "Could not find any lights"
-      end
-    end
-    let(:light) { $lifx.lights.values.first }
-
-    def wait_until(timeout = 1, &block)
-      Timeout.timeout(timeout) do
-        while !block.call
-          sleep 0.1
-        end
-      end
-    rescue Timeout::Error
-      $stderr.puts("Timeout exceeded")
-    end
-
+  describe Light, integration: true do
     describe '#set_color' do
       it "sets the color of the light" do
         color = Color.hsb(rand(360), rand, rand)
         light.set_color(color, 0)
         sleep 0.26
         light.refresh
-        $lifx.flush
+        flush
         wait_until { light.color == color }
         light.color.should == color
       end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,50 @@
 require 'rspec'
 require 'lifx'
+
+shared_context 'integration', integration: true do
+  def lifx
+    $lifx ||= begin
+      options = if ENV['DEBUG']
+        {logger: Yell.new(STDERR)}
+      else
+        {}
+      end
+      c = LIFX::Client.new(options)
+      c.discover
+      begin
+        Timeout.timeout(5) do
+          while c.lights.empty?
+            sleep 0.5
+          end
+        end
+      rescue Timeout::Error
+        raise "Could not find any lights"
+      end
+      c
+    end
+  end
+
+  def wait_until(timeout = 1, &block)
+    Timeout.timeout(timeout) do
+      while !block.call
+        sleep 0.1
+      end
+    end
+  rescue Timeout::Error
+    $stderr.puts("Timeout exceeded")
+  end
+
+  def flush
+    lifx.flush
+  end
+
+  let(:site) { lifx.sites.first }
+  let(:lights) { site.lights }
+  let(:light) { lights.first }
+end
+
+RSpec.configure do |config|
+  config.formatter = 'documentation'
+  config.color = true
+end
+
