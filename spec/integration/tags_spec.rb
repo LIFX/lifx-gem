da31b710183270bd988836d3ea7ad601c714dcd0
Jack Chen (chendo) <github@chen.do>
2014-02-28 16:33:00 +1100

    Add a tags integration spec

---
+++ spec/integration/tags_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+module LIFX
+  describe "tags", integration: true do
+    it 'Clearing, setting and using tags' do
+      sleep(2)
+      light.tags.each do |tag_label|
+        light.remove_tag(tag_label)
+      end
+
+      light.tags.should be_empty
+
+      light.add_tag('Test')
+
+      lifx.lights.with_tag('Test').turn_on
+      color = Color.hsb(rand(360), 0.3, 0.3)
+      lifx.lights.with_tag('Test').set_color()
+      sleep 1
+      light.color.should == color
+    end
+  end
+end
