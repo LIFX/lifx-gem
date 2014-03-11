88a8ad02e96d9fd68937b03d41366eaa0bf67b09
Jack Chen (chendo) <github@chen.do>
2014-03-11 18:05:02 +1100

    Turns out there's more to colour at this point. Pulling it out for now

--- spec/color_spec.rb
+++ spec/color_spec.rb
@@ -3,43 +3,5 @@ require 'spec_helper'
 module LIFX
   describe Color do
     let(:default_kelvin) { 3500 }
-    describe '.rgb' do
-      context 'translating from RGB' do
-        it 'translates red correctly' do
-          c = Color.rgb(255, 0, 0)
-          c.to_a.should == [0, 1, 1, default_kelvin]
-        end
-
-        it 'translates yellow correctly' do
-          c = Color.rgb(255, 255, 0)
-          c.to_a.should == [60, 1, 1, default_kelvin]
-        end
-
-        it 'translates green correctly' do
-          c = Color.rgb(0, 255, 0)
-          c.to_a.should == [120, 1, 1, default_kelvin]
-        end
-
-        it 'translates cyan correctly' do
-          c = Color.rgb(0, 255, 255)
-          c.to_a.should == [180, 1, 1, default_kelvin]
-        end
-
-        it 'translates blue correctly' do
-          c = Color.rgb(0, 0, 255)
-          c.to_a.should == [240, 1, 1, default_kelvin]
-        end
-
-        it 'translates white correctly' do
-          c = Color.rgb(255, 255, 255)
-          c.to_a.should == [0, 0, 1, default_kelvin]
-        end
-
-        it 'translates black correctly' do
-          c = Color.rgb(0, 0, 0)
-          c.to_a.should == [0, 0, 0, default_kelvin]
-        end
-      end
-    end
   end
 end
