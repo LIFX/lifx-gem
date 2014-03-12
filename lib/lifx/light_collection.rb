296dea8ff258621b359a9cc8b35c5073fa64c6e9
Jack Chen (chendo) <github@chen.do>
2014-03-12 18:29:29 +1100

    Document LightCollection

--- lib/lifx/light_collection.rb
+++ lib/lifx/light_collection.rb
@@ -1,19 +1,35 @@
 require 'lifx/light_target'
 
 module LIFX
+  # LightCollection represents a collection of {Light}s, which can either refer to
+  # all lights on a {NetworkContext}, or lights
   class LightCollection
     include LightTarget
+    include Enumerable
     extend Forwardable
 
-    # Represents a collection of lights
-    attr_reader :tag, :context
+    # Refers to {NetworkContext} the instance belongs to
+    # @return [NetworkContext]
+    attr_reader :context
 
-    def initialize(context:, tag: nil, lights: context.all_lights)
+    # Tag of the collection. `nil` represents all lights
+    # @return [String]
+    attr_reader :tag
+
+    # Creates a {LightCollection} instance. Should not be used directly.
+    # @api private
+    # @param context: [NetworkContext] NetworkContext this collection belongs to
+    # @param tag: [String] Tag 
+    def initialize(context:, tag: nil)
       @context = context
       @tag = tag
-      @lights = tag ? lights.select { |l| l.tags.include?(tag) } : lights
+      @lights = tag ? context.all_lights.select { |l| l.tags.include?(tag) } : context.all_lights
     end
 
+    # Queues a {Protocol::Payload} to be sent to bulbs in the collection
+    # @param payload [Protocol::Payload] Payload to be sent
+    # @api private
+    # @return [LightCollection] self for chaining
     def send_message(payload)
       if tag
         context.send_message(target: Target.new(tag: tag), payload: payload)
@@ -23,10 +39,16 @@ module LIFX
       self
     end
 
+    # Returns a {Light} with device id matching `id`
+    # @param id [String] Device ID
+    # @return [Light]
     def with_id(id)
       @lights.find { |l| l.id == id}
     end
 
+    # Returns a {Light} with its label matching `label`
+    # @param label [String, Regexp] Label
+    # @return [Light]
     def with_label(label)
       if label.is_a?(Regexp)
         @lights.find { |l| l.label =~ label }
@@ -35,19 +57,26 @@ module LIFX
       end
     end
 
+    # Returns a {LightCollection} of {Light}s tagged with `tag`
+    # @param tag [String] Tag
+    # @return [LightCollection]
     def with_tag(tag)
-      self.class.new(context: context, tag: tag, lights: lights)
+      self.class.new(context: context, tag: tag)
     end
 
+    # Returns an Array of {Light}s
+    # @return [Array<Light>]
     def lights
-      @lights
+      @lights.dup
     end
 
+    # Returns a nice string representation of itself
+    # @return [String]
     def to_s
       %Q{#<#{self.class.name} lights=#{lights}#{tag ? " tag=#{tag}" : ''}>}
     end
     alias_method :inspect, :to_s
 
-    def_delegators :lights, :empty?, :length, :count, :to_a, :[], :find, :each, :first, :last, :map, :to_a
+    def_delegators :lights, :empty?, :each
   end
 end
