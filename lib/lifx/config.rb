c4d67141cdcd2d62d9ca1d1b28c4ab66b4655a32
Jack Chen (chendo) <github@chen.do>
2014-03-15 15:03:35 +1100

    Add allowed transports option

--- lib/lifx/config.rb
+++ lib/lifx/config.rb
@@ -4,6 +4,7 @@ module LIFX
   Config = Configatron::Store.new
 
   Config.default_duration = 1
+  Config.allowed_transports = [:udp, :tcp]
   Config.logger = Yell.new do |logger|
     logger.level = 'gte.warn'
     logger.adapter STDERR, format: '%d [%5L] %p/%t : %m'
