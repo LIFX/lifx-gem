8fa89df23e4ea59bd23da817f59a49f7cbd6e76c
Jack Chen (chendo) <github@chen.do>
2014-03-31 12:43:05 +1100

    Use namespaces in UDP spec

--- spec/transport/udp_spec.rb
+++ spec/transport/udp_spec.rb
@@ -1,39 +1,42 @@
 require 'spec_helper'
 
-describe LIFX::Transport::UDP, integration: true do
-  subject(:udp) { LIFX::Transport::UDP.new(host, port) }
+module LIFX
+  describe Transport::UDP, integration: true do
+    subject(:udp) { Transport::UDP.new(host, port) }
 
-  let(:host) { 'localhost' }
-  let(:message) { double }
-  let(:port) { 45_828 }
+    let(:host) { 'localhost' }
+    let(:message) { double }
+    let(:port) { 45_828 }
 
-  describe '#write' do
-    let(:payload) { double }
+    describe '#write' do
+      let(:payload) { double }
 
-    it 'writes a Message to specified host' do
-      expect(message).to receive(:pack).and_return(payload)
-      expect_any_instance_of(UDPSocket).to receive(:send)
-                                           .with(payload, 0, host, port)
-      udp.write(message)
-    end
-  end
-
-  describe '#listen' do
-    let(:raw_message) { 'some binary data' }
-    let(:socket) { UDPSocket.new }
-
-    it 'listens to the specified socket data, unpacks it and notifies observers' do
-      messages = []
-      udp.add_observer(self) do |message: nil, ip: nil, transport: nil|
-        messages << message
+      it 'writes a Message to specified host' do
+        expect(message).to receive(:pack).and_return(payload)
+        expect_any_instance_of(UDPSocket).to receive(:send)
+                                             .with(payload, 0, host, port)
+        udp.write(message)
       end
-      udp.listen
+    end
 
-      expect(LIFX::Message).to receive(:unpack)
-                               .with(raw_message)
-                               .and_return(message)
-      socket.send(raw_message, 0, host, port)
-      wait { expect(messages).to include(message) }
+    describe '#listen' do
+      let(:raw_message) { 'some binary data' }
+      let(:socket) { UDPSocket.new }
+      let(:messages) { [] }
+      before do
+        udp.add_observer(self) do |message: nil, ip: nil, transport: nil|
+          messages << message
+        end
+        udp.listen
+      end
+
+      it 'listens to the specified socket data, unpacks it and notifies observers' do
+        expect(Message).to receive(:unpack)
+                                 .with(raw_message)
+                                 .and_return(message)
+        socket.send(raw_message, 0, host, port)
+        wait { expect(messages).to include(message) }
+      end
     end
   end
 end
